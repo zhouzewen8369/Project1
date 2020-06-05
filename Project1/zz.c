3b9d17ef4abd60c0323a74246bf7c729b01e6063
zhou <44389211+zhouzewen8369@users.noreply.github.com>
2020-06-05 21:40:57 +0800

    switch +-计算器

--- Project1/zz.c
+++ Project1/zz.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 //试试注释
 #if 0
-
 int main() {
 	printf("Hello C and VS"); 
 	return -1;
@@ -9,7 +8,6 @@ int main() {
 #endif
 
 #if 0
-
 int main() {
 	int i = 1, sum = 0;
 	do {
@@ -22,8 +20,7 @@ int main() {
 }
 #endif
 
-#if 1
-
+#if 0
 int main() {
 	int i = 1, sum = 0;
 	while (i <= 100) {
@@ -33,5 +30,24 @@ int main() {
 	}
 	printf("1到10的和:%4d\n", sum);
 	return -1;
+}
+#endif
+
+#if 1
+int main() {
+	int a, b;
+	char op;
+	scanf("%d %c %d",&a,&op,&b);
+	switch(op){
+		case '+':
+			printf("%d %c %d的结果是:%d\n", a, op, b, a + b);
+			break;
+		case '-':
+			printf("%d %c %d的结果是:%d\n", a, op, b, a - b);
+			break;
+		default:
+			printf("can't recognize");
+			break;
+	}
 }
 #endif
