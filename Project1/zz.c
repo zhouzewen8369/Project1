245087733d20c2a7a3352c3ed5d4d0eb520fcb19
zhou <44389211+zhouzewen8369@users.noreply.github.com>
2020-06-05 11:20:01 +0800

    while实现连加

--- Project1/zz.c
+++ Project1/zz.c
@@ -1,14 +1,14 @@
-
-#if 0
 #include <stdio.h>
+#if 0
+
 int main() {
 	printf("Hello C and VS"); 
 	return -1;
 }
 #endif
 
-#if 1
-#include <stdio.h>
+#if 0
+
 int main() {
 	int i = 1, sum = 0;
 	do {
@@ -17,6 +17,20 @@ int main() {
 		printf("i = %3d,sum = %d\n", i, sum);
 	} while (i <= 100);
 	printf("1到10的和:%4d\n",sum);
+	return -1;
+}
+#endif
+
+#if 1
+
+int main() {
+	int i = 1, sum = 0;
+	while (i <= 100) {
+		sum += i;
+		++i;
+		printf("i = %3d,sum = %d\n", i, sum);
+	}
+	printf("1到10的和:%4d\n", sum);
 	return -1;
 }
 #endif
