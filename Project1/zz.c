3fe43a699991182312ff0604b0d5a27f65ade44d
zhou <44389211+zhouzewen8369@users.noreply.github.com>
2020-06-05 11:15:06 +0800

    连加

--- Project1/zz.c
+++ Project1/zz.c
@@ -1,5 +1,22 @@
+
+#if 0
 #include <stdio.h>
 int main() {
 	printf("Hello C and VS"); 
 	return -1;
 }
+#endif
+
+#if 1
+#include <stdio.h>
+int main() {
+	int i = 1, sum = 0;
+	do {
+		sum += i;
+		++i;
+		printf("i = %3d,sum = %d\n", i, sum);
+	} while (i <= 100);
+	printf("1µ½10µÄºÍ:%4d\n",sum);
+	return -1;
+}
+#endif
