050f6b050d14cfdd26e29d2d1d5d4b63410ac5f4
zhou <44389211+zhouzewen8369@users.noreply.github.com>
2020-06-06 11:48:25 +0800

    array数组；sizeof

--- Project1/zz.c
+++ Project1/zz.c
@@ -52,7 +52,7 @@ int main() {
 }
 #endif
 
-#if 1
+#if 0
 int main() {
 	float a; 
 	scanf("%f", &a);
@@ -67,3 +67,43 @@ int main() {
 	}
 }
 #endif
+
+#if 1
+//array
+#include <stdio.h>
+int main() {
+
+	//array[n]
+	int a[5];
+	a[1] = 4;
+	for (int i = 0; i < 5; i++) {
+		a[i] = i;
+		printf("%d\t", a[i]);
+	}
+	printf("\n");
+
+	//array[n]={0,1,2,3,4}
+	int a1[5] = { 0,1,2,3,4 };
+	for (int i = 0; i < 5; i++) {
+		printf("%d\t", a1[i]);
+	}
+	printf("\n");
+
+	//array[]={0,1,2,3,4}
+	int a2[] = { 0,1,2,3,4 };
+	int len_a2 = sizeof(a2);
+	for (int i = 0; i < sizeof(a2); i++) {
+		printf("%d\t", a2[i]);
+		//索引“19”超出了“0”至“4”的有效范围(对于可能在堆栈中分配的缓冲区“a2”)。
+	}
+	printf("\nsizeof(a2):%d\n", len_a2);
+
+	//array[n]= for定义
+	int a3[5];
+	for (int i = 0; i < 5; i++) {
+		a3[i] = 2 * i + 5;
+		printf("%d\t", a3[i]);
+	}
+	printf("\n");
+}
+#endif 
