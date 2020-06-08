88d98a3b52224ff15ff19199d3440c274b7d1403
zhou <44389211+zhouzewen8369@users.noreply.github.com>
2020-06-08 18:21:44 +0800

    6 指针；7 结构体； 添加说明，按视频分节标注

--- Project1/zz.c
+++ Project1/zz.c
@@ -1,5 +1,9 @@
-#include <stdio.h>
+//Created by zhouzw
+//Learn C in Visual Studio by watching hw-dong's videos in Bilibili 
+//https://www.bilibili.com/video/BV1Wt41127xf
+//Thanks a lot.
 
+#include <stdio.h>
 //main，printf，return
 #if 0
 int main() {
@@ -8,55 +12,7 @@ int main() {
 }
 #endif
 
-//循环 for也可以
-//do-while循环，1到100求和
-#if 0
-int main() {
-	int i = 1, sum = 0;
-	do {
-		sum += i;
-		++i;
-		printf("i = %3d,sum = %d\n", i, sum);
-	} while (i <= 100);
-	printf("1到10的和:%4d\n",sum);
-	return -1;
-}
-#endif
-//while循环，1到100求和
-#if 0
-int main() {
-	int i = 1, sum = 0;
-	while (i <= 100) {
-		sum += i;
-		++i;
-		printf("i = %3d,sum = %d\n", i, sum);
-	}
-	printf("1到10的和:%4d\n", sum);
-	return -1;
-}
-#endif
-
-//switch-case，计算器，不同运算符
-#if 0
-int main() {
-	int a, b;
-	char op;
-	scanf("%d %c %d",&a,&op,&b);
-	switch(op){
-		case '+':
-			printf("%d %c %d的结果是:%d\n", a, op, b, a + b);
-			break;
-		case '-':
-			printf("%d %c %d的结果是:%d\n", a, op, b, a - b);
-			break;
-		default:
-			printf("can't recognize");
-			break;
-	}
-}
-#endif
-
-//if，判断成绩是否及格
+//3 控制语句-条件 if，判断成绩是否及格
 #if 0
 int main() {
 	float a; 
@@ -72,8 +28,54 @@ int main() {
 	}
 }
 #endif
+//3 控制语句-条件 switch-case，计算器，不同运算符
+#if 0
+int main() {
+	int a, b;
+	char op;
+	scanf("%d %c %d", &a, &op, &b);
+	switch (op) {
+	case '+':
+		printf("%d %c %d的结果是:%d\n", a, op, b, a + b);
+		break;
+	case '-':
+		printf("%d %c %d的结果是:%d\n", a, op, b, a - b);
+		break;
+	default:
+		printf("can't recognize");
+		break;
+	}
+}
+#endif
+//3 控制语句-循环 do-while、while、for，1到100求和
+#if 0
+void main() {
+	int i = 1, sum = 0, n = 3;
+	do {
+		sum += i;
+		++i;
+		printf("i = %3d, sum = %3d\n", i, sum);
+	} while (i <= n);
+	printf("1到%d的和: %d\n", n, sum);
 
-//array，数组
+	i = 1, sum = 0, n = 10;
+	while (i <= n) {
+		sum += i;
+		++i;
+		printf("i = %3d, sum = %3d\n", i, sum);
+	}
+	printf("1到%d的和: %d\n", n, sum);
+
+	i = 1, sum = 0, n = 100;
+	for (i = 1; i <= n; i++) {
+		sum += i;
+		printf("i = %3d, sum = %3d\n", i, sum);
+	}
+	printf("1到%d的和: %d\n", n, sum);
+}
+#endif
+
+//4 array，数组
 #if 0
 //array
 #include <stdio.h>
@@ -116,7 +118,7 @@ int main() {
 	printf("\n");
 }
 #endif 
-//输入score并汇总为数组scores
+//4 输入score并汇总为数组scores
 #if 0
 #include <stdio.h>
 int main() {
@@ -141,7 +143,7 @@ int main() {
 }
 #endif
 
-//定义函数，单个参数，define function 
+//5 定义函数，单个参数，define function 
 #if 0
 //返回值，函数名，形参
 void f(int n) {  //void 空的，无效的
@@ -156,7 +158,7 @@ int main() {
 	return 1;
 }
 #endif
-//定义函数add两个数相加
+//5 定义函数add两个数相加
 #if 0
 
 #include <stdio.h>
@@ -169,7 +171,7 @@ int main() {
 	add(x, y);
 }
 #endif
-//判断是否为质数，for、if、while
+//5 判断是否为质数，for、if、while
 #if 0
 void zhishu(int x) {
 	int flag = 0;
@@ -201,7 +203,7 @@ int main() {
 	}
 }
 #endif
-//输出100以内的所有质数
+//5 输出100以内的所有质数
 #if 0
 int zhishu(int x) {
 	int flag = 0;
@@ -220,8 +222,7 @@ int main() {
 	}
 }
 #endif
-
-//for循环，阶乘计算
+//5 for循环，阶乘计算
 #if 0
 int jiecheng(int x) {
 	int y = 1;
@@ -236,13 +237,18 @@ int main() {
 	printf("%d", jiecheng(x));
 }
 #endif
-//递归，阶乘计算
+//5 递归，阶乘计算
 #if 0 
+//递归("套娃") 
+//1.基情形。任务不能再分解。最小的"套娃"  
+//2.分解情形。把规模大的转化为规模小的，拆套娃。通过循环把大的一层一层的嵌套上去。
 int jiecheng(int x) {
+	//最小套娃
 	int y = 1;
 	if ((x == 1) || (x == 0)) {
 		return y;
-	}	
+	}
+	//套自己，闹太套，就硬套
 	else if (x > 1) {
 		y = x * jiecheng(x - 1);
 		return y;
@@ -258,8 +264,8 @@ int main() {
 }
 #endif
 
-//指针，交换两数，通过指针变量赋值，字符数组、整数数组
-#if 1
+//6 指针，交换两数，通过指针变量赋值，字符数组、整数数组
+#if 0
 #include <string.h>
 void swap(int* x, int* y) {
 	int temp;
@@ -275,11 +281,14 @@ int main() {
 	swap(&a, &b);
 	printf("交换后：%d, %d\n", a, b);
 
+	printf("%d, %d\n", a, *d);
+	//打印地址
+	printf("%p, %p\n", &a, d);
 	//通过指针变量赋值
 	*d = 5;
-	printf("%d, %d\n", a,b);
-	b = *d;
-	printf("%d, %d\n", a, b);
+	printf("%d, %d, %d\n", a, *d, b);
+	b = *d + 2;
+	printf("%d, %d, %d\n", a, *d, b);
 
 	printf("原来的：%c, %c\n", *p, crr[0]);
 	crr[0] = 'H';
@@ -289,4 +298,201 @@ int main() {
 	}
 	printf("\n");
 }
+#endif
+//6 指针，通过指针变量 访问整数数组 并逐个打印， *(p+2)相当于p[2]；
+#if 0
+int main() {
+	int arr[5] = { 0,1,2,3,4 };
+	int* p = arr; // 注意！！int* p = &arr[0]
+	printf("%p, %p, %p\n", p, arr, &arr[0]);
+
+	printf("%d, %d\n", p[0], arr[0]);
+	printf("%d, %d\n", *p, *arr);
+
+	//注意！！*(p+2)相当于p[2]
+	printf("%d, %d\n", *p + 2, arr[2]);
+	printf("%d, %d\n", p[2], *arr+2);
+	int b = p[3];
+	printf("%d, %d\n", b, arr[3]);
+
+	//while、for循环，逐个打印数组元素
+	while (p < &arr[5]) {
+		printf("%d  ", *p);
+		p++;
+	}
+	printf("\n");
+	for (p = arr; p < &arr[5]; p++) { 
+	//这里p需要重新初始化
+		printf("%d  ", *p);
+	}
+	printf("\n");
+	p -= 2; //for循环完后 p=&arr[5]
+	printf("%d\n", *p); //输出3
+}
+#endif
+//6 自定义Print函数，再玩一玩指针p
+#if 0
+#include <stdlib.h>
+//定义 逐个打印字符数组 的函数
+void Print(char crr[],int n) {
+	for (char*p = crr; p < &crr[n]; p++) {
+		printf("%c  ", *p);
+	}
+	printf("\n");
+}
+int main() {
+	int a = 6; char s[6];
+	_itoa(a, s, 2);  //_itoa: int to array
+	printf("%s\n", s);
+	Print(s, 5);
+
+	char crr[] = { 'h','e','l','l','o' }, ch, * p = &ch;
+	*p = 'H'; //即 ch = 'H';
+	crr[0] = ch;
+	Print(crr, 5);
+
+	p = crr; // p = &crr[0]；
+	*(p + 2) = 'L'; //并未改变p！！
+	Print(crr, 5);
+
+	p++; //此时 p = &crr[1]；
+	*p = 'E';
+	Print(crr, 5);
+}
+#endif
+//6 通过指针 将字符数组hello 改成ABCDE
+#if 0
+void main() {
+	char crr[] = { 'h','e','l','l','o' }, * p, * q;
+	p = crr;
+	q = crr + 5;
+	while (p < q) {
+		printf("%c\t", *p);
+		p++;
+	}
+	printf("\n");
+	p = crr;
+	*p = 'A';
+	p++;
+	while (p < q) {
+		*p = *(p - 1) + 1;
+		p++;
+	}
+	for (p = crr; p < q; p++) {
+		printf("%c\t", *p);
+	}
+}
+#endif
+//6 字符串，结束字符'\0'; C语言中 字符用单引号，字符串用双引号；自定义Strcpy函数；
+#if 0
+#include <string.h> //<string.h>字符串处理函数的头文件;
+void Print(char crr[], int n) {
+	//crr[]:字符数组、字符串的指针,注意是指针！相当于 char* crr； 
+	//n:希望显示的字符个数
+	for (char* p = crr; p < &crr[n]; p++) {
+		printf("%c  ", *p);
+	}
+	printf("\n");
+}
+//尝试自定义strcpy，指针移动，逐个赋值，实现复制
+void Strcpy(char* crr, char* s) {
+	//功能：将*s复制给*crr
+	char* p = crr; //char* p = &crr[0];
+	char* q = s;
+	while (*q != '\0') {
+		*p = *q;
+		p++;
+		q++;
+	}
+}
+void main() {
+	char arr[] = { 'h','e','l','l','o' };
+	char str[] = { 'h','e','l','l','o','\0' };
+	char* p = "hello";
+	printf("%zd, %zd\n", strlen(arr), strlen(str));
+	Print(arr, 5); //使用自定义Print函数
+	Print(str, 5); //同样可以使用
+	Print(p, 5);
+	printf("%s\n", arr); //出现乱码
+	printf("%s\n", str); //'\0'起了作用
+	printf("%s\n", p); //显然对应的 char* p = "hello"; 这种定义方式最简便；
+
+	strcpy(str, "WORLD"); printf("%s\n", str);
+	//strcpy(arr, "WORLD"); printf("%s\n", arr);  //报错
+	arr[4] = '\0'; //将最后一个字符修改为'\0'后，使用strcpy不报错
+	strcpy(arr + 2, "LL"); printf("%s\n", arr);
+
+	Strcpy(str, &"yes"[0]); //Strcpy(str, "yes"); 
+	printf("%s\n", str);
+}
+#endif
+
+//7 struct结构体，学生信息；
+#if 0
+#include<stdio.h>
+#include<string.h>
+
+struct Stu {
+	char name[10]; 
+	int age;
+	double score;
+};
+int main() {
+	struct Stu zzw;
+	struct Stu* p;
+	p = &zzw;
+	
+	//zzw.
+	strcpy(zzw.name, "zhouzw"); //注意！！name是指针,要使用strcpy
+	zzw.age = 24;
+	zzw.score = 89.35;
+	printf("1---%d岁的%s的成绩是%5.2lf\n", zzw.age, zzw.name, zzw.score);
+	//(*p).
+	strcpy((*p).name, "zhouzw"); 
+	(*p).age = 24;
+	(*p).score = 89.35;
+	printf("2---%d岁的%s的成绩是%5.2lf\n", (*p).age, (*p).name, (*p).score);
+	//p->
+	strcpy(p->name, "zhouzw"); 
+	p->age = 24;
+	p->score = 89.35;
+	printf("3---%d岁的%s的成绩是%5.2lf\n", p->age, p->name, p->score);
+}
+#endif
+//7 struct结构体，录入多个学生信息
+#if 0
+#include<stdio.h>
+#include<string.h>
+//定义结构体
+struct Stu { 
+	char name[10];
+	int age;
+	double score;
+};
+//输入
+void Scanf(struct Stu* p) {
+	printf("依次输入姓名、年龄、分数\n");
+	scanf("%s %d %lf", p->name, &(p->age), &(p->score));
+	//注意！使用scanf时，逗号后面跟 地址或指针，"&"取地址符号不要忘！！
+}
+int main() {
+	int i = 1; //已录入人数
+	struct Stu students[10]; //预计录入人数  这里值得思考改进！后面的动态内存
+	struct Stu* p = students, * q = 0; 
+	while(1) {
+		Scanf(p);
+		if (p->age < 0)
+			break; //跳出循环，结束录入
+		printf("已录入%d个学生信息\n", i);
+		p++; i++; //指针、人数加1，继续录入
+	}
+	q = p; //q用于记录最后一个学生信息的指针
+	for (p = students; p < q; p++)   //把已录入的打印出来
+		printf("%3d岁的%5s的成绩是%5.2lf\n", p->age, p->name, p->score);
+}
+#endif
+
+//
+#if 1
+
 #endif
